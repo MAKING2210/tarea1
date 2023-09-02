d24115db80fc075f87963bfa0005acddb4414a22
MAKING2210 <mariolira23@gmail.com>
2023-09-02 11:35:00 -0400

    modificacion main

--- src/main/java/org/example/Motocicleta.java
+++ src/main/java/org/example/Motocicleta.java
@@ -5,6 +5,28 @@ public class Motocicleta extends Vehiculo{
     public Motocicleta(String marca, int año, double precio, int capacidadEstanque, int cilindrada) {
         super(marca, año, precio, capacidadEstanque);
         this.cilindrada = cilindrada;
+        this.setMarca(marca);
+        this.setAño(año);
+        this.setPrecio(precio);
+        this.setCapacidadEstanque(capacidadEstanque);
+        this.cilindrada = cilindrada;
+    }
+
+    public String getMarca(String marca) {
+        return marca;
+    }
+
+    public int getAño(int año) {
+        return año;
+    }
+
+    public double getPrecio(double precio) {
+        return precio;
+    }
+
+    public int getCapacidadEstanque(int capacidadEstanque) {
+        return capacidadEstanque;
+
     }
     public int getCilindrada() {
         return cilindrada;
@@ -12,9 +34,14 @@ public class Motocicleta extends Vehiculo{
     public void setCilindrada(int cilindrada) {
         this.cilindrada = cilindrada;
     }
-    @Override
     public void ImprimirDatos() {
+        System.out.println("Marca: " + getMarca());
+        System.out.println("Año: " + getAño());
+        System.out.println("Precio: " + getPrecio());
+        System.out.println("Capacidad Estanque: " + getCapacidadEstanque());
+        System.out.println("Nivel Estanque: " + getNivelEstanque());
     }
+
     @Override
     public void VaciarEstanque(int litros){
 }
